c138bcf5c290936d31b4c516857f5ecc39c9418a
silvertouchltd <36691487+silvertouchltd@users.noreply.github.com>
2018-06-11 17:01:25 +0530

    Add files via upload

---
+++ tests/TestCase/Controller/MenuRegionsControllerTest.php
@@ -0,0 +1,75 @@
+<?php
+namespace CakeSilverCms\Test\TestCase\Controller;
+
+use CakeSilverCms\Controller\MenuRegionsController;
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * CakeSilverCms\Controller\MenuRegionsController Test Case
+ */
+class MenuRegionsControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.cake_silver_cms.menu_regions',
+        'plugin.cake_silver_cms.menus',
+        'plugin.cake_silver_cms.objects',
+        'plugin.cake_silver_cms.modules',
+        'plugin.cake_silver_cms.widgets'
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     */
+    public function testView()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     */
+    public function testAdd()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     */
+    public function testEdit()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
